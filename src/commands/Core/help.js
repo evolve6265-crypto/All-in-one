0bbb643115ebabe52f8c80aa97c9fbff418a8567
evolve6265-crypto <evolve6265@gmail.com>
2026-05-04 15:47:14 +0530

    Update support and touchpoint button URLs

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -169,12 +169,12 @@ export async function createInitialHelpMenu(client) {
 
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
+        .setURL("https://discord.gg/8XHSMAXdk")
         .setStyle(ButtonStyle.Link);
 
     const touchpointButton = new ButtonBuilder()
         .setLabel("Learn from Touchpoint")
-        .setURL("https://www.youtube.com/@TouchDisc")
+        .setURL("https://discord.gg/8XHSMAXdk")
         .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
